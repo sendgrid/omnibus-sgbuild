dbc14f3316d0f5836ac513cd286c9f65ee555e0b
Ty Alexander <ty.alexander@sendgrid.com>
2014-10-14 23:53:28 -0600

    initial commit

---
+++ config/projects/sgbuild.rb
@@ -0,0 +1,26 @@
+name 'sgbuild'
+maintainer 'SendGrid'
+homepage 'https://github.com/sendgrid/omnibus-sgbuild'
+
+install_dir "/opt/#{name}"
+build_version do
+  # Use chef to determine the build version
+  source :git, from_dependency: 'sgbuild'
+
+  # Output a SemVer compliant version string
+  output_format :semver
+end
+build_iteration 1
+
+# creates required build directories
+dependency 'preparation'
+
+# sgbuild dependencies/components
+dependency 'sgbuild'
+
+# version manifest file
+dependency 'version-manifest'
+
+exclude '\.git*'
+exclude 'bundler\/git'
+
---
+++ config/software/sgbuild.rb
@@ -0,0 +1,16 @@
+name "sgbuild"
+default_version "0.1.2"
+
+dependency "ruby"
+dependency "rubygems"
+dependency "bundler"
+
+source :git => "git@github.com:sendgrid/sgbuild.git"
+
+relative_path "sgbuild"
+
+build do
+  env = with_embedded_path()
+  bundle "install --without development_extras", env: env
+  bundle "exec rake install", env: env
+end
